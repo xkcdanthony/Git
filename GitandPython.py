94cb9cfe46e0a6e7d202d678fd63bbf6d8735d4b
xkcdanthony <anthony.cross.7060@mail.linnbenton.edu>
2023-03-23 16:21:11 -0700

    My Code

---
+++ GitandPython.py
@@ -0,0 +1,12 @@
+# This is going to be my practice for creating a .py file and then using Git to use branching,committing, and pushing to GitHub for a public repository
+
+
+name = input("what is the name of this user? ")
+
+if name == "Anthony Cross":
+    print("Welcome back boss!")
+
+elif input != "name":
+    print("User not recognized...RED ALERT!")
+
+input()
