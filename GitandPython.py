5ac7260653e506b8ca1274dc16734ea5d1b81456
xkcdanthony <anthony.cross.7060@mail.linnbenton.edu>
2023-03-23 17:19:46 -0700

    Added comment to line 3

--- GitandPython.py
+++ GitandPython.py
@@ -1,5 +1,6 @@
 # This is going to be my practice for creating a .py file and then using Git to use branching,committing, and pushing to GitHub for a public repository
 
+# I added this comment for branching and showing that I edited the GitandPython.py file
 
 name = input("what is the name of this user? ")
 
